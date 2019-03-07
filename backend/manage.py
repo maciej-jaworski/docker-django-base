5ba68d044ecbbe0d7abbd8bf1732eccc84e67d12
Maciej Jaworski <maciej.jaworski@tivix.com>
2019-03-07 11:56:14 +0100

    Enable VSCode debugger

--- backend/manage.py
+++ backend/manage.py
@@ -2,6 +2,7 @@
 import os
 import sys
 
+
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
     try:
@@ -12,4 +13,10 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    if 'runserver' in sys.argv and os.environ.get('RUN_MAIN'):
+        import ptvsd
+
+        ptvsd.enable_attach(address=("0.0.0.0", 3000))
+
     execute_from_command_line(sys.argv)
